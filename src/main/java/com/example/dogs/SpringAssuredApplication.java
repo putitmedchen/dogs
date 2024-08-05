1f958882a45709c394cc449b48663e9f76d70a97
karr.agency <karr.agency@gmail.com>
2024-08-05 11:20:28 +0300

    Created web api projects with mock mvc

---
+++ src/main/java/com/example/dogs/SpringAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dogs/HttpRequestTest.java
@@ -0,0 +1,26 @@
+package com.example.dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort ()
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+}
